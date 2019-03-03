1b79cca6a4c75dcfe89030b0ceb28e9d9f5b0ca6
Pedro Guimarães <moisespedro15@gmail.com>
2019-03-03 00:16:45 -0300

    Remove SplashActivity

--- app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
+++ app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.arctouch.codechallenge.home
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import com.arctouch.codechallenge.R
@@ -8,23 +9,31 @@ import com.arctouch.codechallenge.base.BaseActivity
 import com.arctouch.codechallenge.data.Cache
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.home_activity.*
+import kotlinx.android.synthetic.main.home_activity.progressBar
+import kotlinx.android.synthetic.main.home_activity.recyclerView
 
 class HomeActivity : BaseActivity() {
 
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
 
-        api.upcomingMovies(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE, 1, TmdbApi.DEFAULT_REGION)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe {
-                val moviesWithGenres = it.results.map { movie ->
-                    movie.copy(genres = Cache.genres.filter { movie.genreIds?.contains(it.id) == true })
+        api.genres(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    Cache.cacheGenres(it.genres)
+                    api.upcomingMovies(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE, 1, TmdbApi.DEFAULT_REGION)
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe { it1 ->
+                                val moviesWithGenres = it1.results.map { movie ->
+                                    movie.copy(genres = Cache.genres.filter { movie.genreIds?.contains(it.id) == true })
+                                }
+                                recyclerView.adapter = HomeAdapter(moviesWithGenres)
+                                progressBar.visibility = View.GONE
+                            }
                 }
-                recyclerView.adapter = HomeAdapter(moviesWithGenres)
-                progressBar.visibility = View.GONE
-            }
     }
 }
